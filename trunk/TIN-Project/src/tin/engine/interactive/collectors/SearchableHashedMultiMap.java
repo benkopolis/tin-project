52c608b8dae31c3e2537d455b576a462155ed2f6
benkopolis <benkopolis@ec564538-3c0f-11de-b8bc-ddcf2599578a>
2009-06-03 19:09:14 +0000

    SearchableHashedMultiMap zakodzone - pizza hut w srode przed godzina 21

--- trunk/TIN-Project/src/tin/engine/interactive/collectors/SearchableHashedMultiMap.java
+++ trunk/TIN-Project/src/tin/engine/interactive/collectors/SearchableHashedMultiMap.java
@@ -3,9 +3,12 @@
  */
 package tin.engine.interactive.collectors;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import tin.engine.data.Data;
+import tin.engine.exceptions.TINException;
 import tin.engine.interactive.Interactive;
 
 /**
@@ -16,7 +19,8 @@ import tin.engine.interactive.Interactive;
 public final class SearchableHashedMultiMap extends Interactive
 {
 
-	
+	HashMap<Object, Vector<Object>> keyToVal = new HashMap<Object, Vector<Object>>();
+	HashMap<Object, Vector<Object>> valToKey = new HashMap<Object, Vector<Object>>();
 
 	/**
 	 * 
@@ -29,8 +33,51 @@ public final class SearchableHashedMultiMap extends Interactive
 	@Override
 	protected Data processData(Data data)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String cmd;
+		Object object;
+		Data temp = new Data();
+		try {
+			temp.addObject("readerID", data.getData("readerID"));
+			cmd = (String) data.getData("command");
+			if (cmd.equalsIgnoreCase("add")) { // dodawanie pary <K, V> do asoscjacji * do *
+				// najpierw do klucz - wartosc dodajemy pod
+				// odpowiedni klucz podan¹ wartosc do wektora wartosci
+				if(keyToVal.containsKey(data.getData("key"))) { 
+					Vector<Object> v = ((Vector<Object>) (keyToVal.get(data.getData("key"))));
+					v.add(data.getData("value"));
+				} else {
+					Vector<Object> v = new Vector<Object>(3);
+					v.add(data.getData("value"));
+					keyToVal.put(data.getData("key"), v);
+				}
+				// nastepnie do pary wartosc - klucz dodajemy pod odpowiednia
+				// wartosc podany klucz do wektora kluczy
+				if(valToKey.containsKey(data.getData("value"))) { 
+					Vector<Object> v = (Vector<Object>) (valToKey.get(data.getData("value")));
+					v.add(data.getData("key"));
+				} else {
+					Vector<Object> v = new Vector<Object>(3);
+					v.add(data.getData("key"));
+					valToKey.put(data.getData("value"), v);
+				}
+				return null;
+			} else if (cmd.equalsIgnoreCase("clear")) {
+				keyToVal.clear();
+				valToKey.clear();
+				return null;
+			} else if (cmd.equalsIgnoreCase("getByKey")) { // wyszukiwanie po kluczu
+				Vector<Object> v = keyToVal.get(data.getData("key"));
+				temp.addObject("result", v); // dodaje referencje na wektor do obiektu data
+				return temp;
+			} else if(cmd.equalsIgnoreCase("getByValue")) { // wyszukiwanie po wartosciach
+				Vector<Object> v = valToKey.get(data.getData("value"));
+				temp.addObject("result", v); // dodaje referencje na wektor do obiektu data
+				return temp;
+			} else return null;	
+		} catch (TINException e) {
+			e.printStackTrace();
+			return null; // TODO na pewno ? ;p
+		}
 	}
 
 	@Override
